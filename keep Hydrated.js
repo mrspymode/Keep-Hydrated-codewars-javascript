f0fd3a4113cd85bf790272e0761d70a185debd49
mrspyyt <piotrus.dudek@op.pl>
2023-01-25 09:33:36 +0100

    adding final code
    finall app and code has been added

---
+++ keep Hydrated.js
@@ -0,0 +1,7 @@
+//working every other place
+const num = prompt('Enter a Time: ');
+const b = 2;
+const res = parseInt(num)/parseInt(b);
+console.log(parseInt(res));
+//working on codewors
+Litres = time => Math.floor(time * 0.5)
